3b27ccfbc22b86d0ad0c1d08abbbfefb33c99349
Lainezz <diegolinaresmojacar@gmail.com>
2023-11-14 13:52:07 +0100

    adicion de codigo de clase

---
+++ HeaderResponsive/logica.js
@@ -0,0 +1,27 @@
+const cambiarDisenio = () => {
+  const navNormal = document.querySelector("header .header__zonaNavegacion");
+  const navHamb = document.querySelector("header .header__zonaNavegacionHamb");
+
+  if(!navNormal.classList.contains("hidden")){
+    navNormal.classList.add("hidden")
+    navHamb.classList.remove("hidden")
+  }
+};
+
+const cambiarDisenio2 = () => {
+  const navNormal = document.querySelector("header .header__zonaNavegacion");
+  const navHamb = document.querySelector("header .header__zonaNavegacionHamb");
+
+  if (navNormal.classList.contains("hidden")) {
+    navHamb.classList.add("hidden");
+    navNormal.classList.remove("hidden");
+  }
+};
+
+window.onresize = () => {
+  if (window.innerWidth <= 600) {
+    cambiarDisenio();
+  } else {
+    cambiarDisenio2();
+  }
+};
