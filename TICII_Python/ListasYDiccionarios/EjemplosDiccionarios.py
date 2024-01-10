e686a1d170da2c971e5af2c90f42f5534bd2a095
Lainezz <diegolinaresmojacar@gmail.com>
2024-01-10 10:22:41 +0100

    adicion de ejemplosdiccionarios

---
+++ TICII_Python/ListasYDiccionarios/EjemplosDiccionarios.py
@@ -0,0 +1,35 @@
+# Definición de diccionario
+divisas = {"Euro": "€", "Dolar": "$", "Bitcoin": "BTC", "Dogecoin": "DGC"}
+
+print("Imprimiendo los elementos de un diccionario")
+print(divisas["Euro"])
+print(divisas["Dolar"])
+print(divisas["Bitcoin"])
+print(divisas["Dogecoin"])
+
+divisas["Dogecoin"] = "DOGE"
+
+print("Imprimiendo los elementos de un diccionario")
+print(divisas["Euro"])
+print(divisas["Dolar"])
+print(divisas["Bitcoin"])
+print(divisas["Dogecoin"])
+
+divisas["Dogecoinn"] = "DOGEE"
+
+print("Imprimiendo los elementos de un diccionario.")
+print(divisas["Euro"])
+print(divisas["Dolar"])
+print(divisas["Bitcoin"])
+print(divisas["Dogecoin"])
+print(divisas["Dogecoinn"])
+
+
+
+"""
+Escribir un programa que guarde en una variable el 
+diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario 
+por una divisa y muestre su símbolo o un mensaje de aviso si la divisa 
+no está en el diccionario.
+"""
+
