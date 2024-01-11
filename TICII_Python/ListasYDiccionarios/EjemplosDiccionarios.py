e9d16b34c1c765ef978c5352d55a73ed0bd26889
Lainezz <diegolinaresmojacar@gmail.com>
2024-01-11 10:19:07 +0100

    adicion de codigo de clase

--- TICII_Python/ListasYDiccionarios/EjemplosDiccionarios.py
+++ TICII_Python/ListasYDiccionarios/EjemplosDiccionarios.py
@@ -1,35 +1,5 @@
 # Definición de diccionario
-divisas = {"Euro": "€", "Dolar": "$", "Bitcoin": "BTC", "Dogecoin": "DGC"}
-
-print("Imprimiendo los elementos de un diccionario")
-print(divisas["Euro"])
-print(divisas["Dolar"])
-print(divisas["Bitcoin"])
-print(divisas["Dogecoin"])
-
-divisas["Dogecoin"] = "DOGE"
-
-print("Imprimiendo los elementos de un diccionario")
-print(divisas["Euro"])
-print(divisas["Dolar"])
-print(divisas["Bitcoin"])
-print(divisas["Dogecoin"])
-
-divisas["Dogecoinn"] = "DOGEE"
-
-print("Imprimiendo los elementos de un diccionario.")
-print(divisas["Euro"])
-print(divisas["Dolar"])
-print(divisas["Bitcoin"])
-print(divisas["Dogecoin"])
-print(divisas["Dogecoinn"])
-
-if divisas.get("Dogecoinnn") == None:
-    print("DogeCoinnn no existe")
-else:
-    print(f'La divisa es: {divisas.get("Dogecoinn")}')
-
-
+divisas = {"Euro": "€", "Dolar": "$", "Bitcoin": "BTC", "Dogecoin": "DOGE"}
 
 """
 Escribir un programa que guarde en una variable el 
@@ -37,4 +7,23 @@ diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario
 por una divisa y muestre su símbolo o un mensaje de aviso si la divisa 
 no está en el diccionario.
 """
+
+# Manera 1
+print("Dime la divisa que quieras comprobar: ")
+divisaUsuario = input()
+
+if divisas.get(divisaUsuario) == None:
+    print("La divisa no se encuentra en el diccionario")
+    respuesta = input("¿le gustaria incluir esa divisa en el diccionario?")
+    if respuesta == "Si" or respuesta == "s":
+        simbolo = input("Indique el simbolo de la divisa")
+        divisas[divisaUsuario] = simbolo
+else: 
+    print(f'El simbolo de la divisa indicada es: {divisas[divisaUsuario]}')
+
+# Imprimir por pantalla el diccionario entero (Clave y Valor)
+# Debemos recorrer el diccionario (MIRANDO LA PIZARRA)
+for clave in divisas:
+    print(clave+": "+divisas[clave])
+
 
