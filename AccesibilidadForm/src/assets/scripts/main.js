ae8747f7f887b2b2177568b59ef666186ef5e74e
Lainezz <diegolinaresmojacar@gmail.com>
2024-03-04 10:03:16 +0100

    adicion de codigo para clase

--- AccesibilidadForm/src/assets/scripts/main.js
+++ AccesibilidadForm/src/assets/scripts/main.js
@@ -1,2 +1,32 @@
-console.log("Hola Mundo")
 
+
+
+
+
+function onIniciar() {
+  console.log("Hola Mundo");
+
+  // Script para cambiar la pestaña en el main
+  const botonesTab = document.querySelectorAll(".listaBotones__botonTab");
+  //console.log(botonesTab)
+
+  // Recorremos la lista de botones para añadir un eventListener de click
+  botonesTab.forEach((boton, i) => {
+    //console.log(boton)
+
+    // El evento consiste en quitar la clase activo a todos los botones
+    // y acto seguido añadir la clase activo únicamente al que se le ha hecho click
+    boton.addEventListener("click", () => {
+      botonesTab.forEach((botonF, i) => {
+        botonF.classList.remove("activo");
+      });
+      boton.classList.add("activo");
+    });
+  });
+
+  const contenedoresTexto = document.querySelectorAll(".contenedorTexto__bloque");
+  //contenedorTexto.addEventListener("click", removeActivo);
+}
+
+
+window.onload = onIniciar();
