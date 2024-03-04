a1999a3dacbc728cc9c26dc86abc1303585487ee
dgutper960 <dgutper960@outlook.es>
2024-03-04 11:30:52 +0100

    activo

--- AccesibilidadForm/src/assets/scripts/main.js
+++ AccesibilidadForm/src/assets/scripts/main.js
@@ -6,6 +6,19 @@
 function onIniciar() {
   console.log("Hola Mundo");
 
+  // 1. Conseguimos todos los botonos
+  const botonesTab = document.querySelectorAll(".botonesTab__pestanya");
+  // 2. Conseguimos todos los bloquews de texto"
+  const bloquesTexto = document.querySelectorAll(".contenedorTexto__bloque");
+  // 3. añadir Evenlistener a todos los botones
+  botonesTab.forEach( (boton, i)=>{
+      console.log(boton); // Cada boton
+
+      boton.addEventListener("click", ()=>{
+        console.log("Click en boton"+(i+1))
+        boton.classList.add("activo");
+      })
+  })
 
 }
 
